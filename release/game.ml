28040c149730d7f5db6f1ae14ca8d498b06aa614
qeb2 <qeb2@cornell.edu>
2014-04-29 00:58:11 +0000

    implemented game type, and conversion functions

--- release/game.ml
+++ release/game.ml
@@ -4,15 +4,16 @@ open Util
 open Print
 
 
-type game = Something | Or | Other
+type game = state (*Is anything else worth including/calculating?*)
 
-let state_of_game g = failwith "A voice said look me in the stars"
-let game_of_state s = failwith "And tell me truly, men of earth,"
+let state_of_game g = g
+let game_of_state s = s
 
 
 let init_game () = game_of_state (gen_initial_state())
+    (*Change to game_of_state(gen_random_initial_state ())*)
 
 
-let handle_move s m = failwith "If all the soul-and-body scars"
+let handle_move g m = failwith "If all the soul-and-body scars"
 
 let presentation g = failwith "Were not too much to pay for birth."
