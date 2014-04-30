ea15301c8c09e5eddbb4185d3edfe69bb4f711ee
qeb2 <qeb2@cornell.edu>
2014-04-30 23:48:28 +0000

    Added gameType.ml

---
+++ release/gameType.ml
@@ -0,0 +1,51 @@
+open Definition
+open Constant
+
+type maprecord = {
+  hexes: hex list;
+  ports: port list
+}
+
+type structurerecord = {
+  settlements: intersection list;
+  roads: road list
+}
+
+type boardrecord = {
+  map: maprecord;
+  structures: structurerecord;
+  deck: cards;
+  discard: card list;
+  robber: piece
+}
+
+type costrecord = {
+  bricks: int;
+  wool: int;
+  ore: int;
+  grain: int;
+  lumber: int
+}
+
+type playerrecord = {
+  inventory: costrecord;
+  cards: cards;
+  knights: int;
+  longestroad: bool;
+  largestarmy: bool;
+}
+
+type nextrecord = {
+  color: color;
+  request: request
+}
+
+type t = {
+  board: boardrecord;
+  blue: playerrecord;
+  red: playerrecord;
+  orange: playerrecord;
+  white: playerrecord;
+  turn: turn;
+  next: color * request
+}
