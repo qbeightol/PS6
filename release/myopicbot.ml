5c58c430c3088c94a401d01e30c66aa892fc8567
qeb2 <qeb2@cornell.edu>
2014-05-05 23:59:22 +0000

    got myopicbot to compile

--- release/myopicbot.ml
+++ release/myopicbot.ml
@@ -72,64 +72,54 @@ module Bot = functor (S : Soul) -> struct
   type values = (float * float * float * float * float)
 
   (*looks at builds to determine the value of resources*)
-  let analyze_build (c: cost), (v: float)) : values = 
+  let analyze_build ((c: cost), (v: float)) : values = 
     let total_cost = sum_cost c in
-    let f r = (float (r / total_cost)) *. v in
+    let f r = (float r) /. float(total_cost) *. v in
       map_cost f c
 
   (*Determines the expected value of a card drawn from the deck*)
   let calc_card_value (s: state) : float = 
     let ((_, _, deck, discard, _), players, _, (c, _)) = s in
     let is_active_player (color, _, _) = (color = c) in
-    let (_, (_, cards), _) = listmem_remove is_active_player players in
+    let (_, (_, cards), _) = List.find is_active_player players in
       let card_list = reveal cards in
       let dealt_vps = list_count ((=) VictoryPoint) (card_list@discard) in
       let remaining_vps = cNUM_VICTORYPOINT - dealt_vps in
       (*Assumes that the game is played with the default deck*)
       let total_cards = List.length (reveal cDEFAULT_DECK) in
       let undrawable_cards = List.length (card_list) + List.length (discard) in
-      let drawable_cards = total_card -undrawable_cards in
-        ((float remaining_vps) /. float(drawable_cards)) *. cVP_CARD
+      let drawable_cards = total_cards -undrawable_cards in
+        ((float remaining_vps) /. float(drawable_cards)) *. float (cVP_CARD)
 
 
+  (*I don't think I'm going to use this
   type resourcevalues = {
-    b = float;
-    w = float;
-    o = float;
-    g = float;
-    l = float
+    b: float;
+    w: float;
+    o: float;
+    g: float;
+    l: float
   }
+  *)
 
 
   (*returns a record containing rough estimates of how much the active player 
   values each resource*)
-  let eval_resources (s: state) : resourcevalues =
-  (* Cost to build the four build types
-  let cCOST_ROAD : cost = (1,0,0,0,1)
-  let cCOST_TOWN : cost = (1,1,0,1,1)
-  let cCOST_CITY : cost = (0,0,3,2,0)
-  let cCOST_CARD : cost = (0,1,1,1,0)
-  
-  let cVP_CARD = 1 (* per victory card *)
-  let cVP_TOWN = 1 (* per town *)
-  let cVP_CITY = 2 (* per city *)
-
-  *)
-
+  let eval_resources (s: state) =
 
     let cVP_ROAD = 0.3 (*A road's approximate worth in victory points--this 
     is kind of handwavy. You'd certainly could get 2 victory points simply by
     owning five roads, but you'd probably need to much more--maybe 10 to 
-    actually have a firm grasp on the trophy. Adding another tenth a point
-    seems reasonable, given that roads do have some strategic value, and in
-    general, its probably good to encourage the bot to build roads, since 
-    they'll help the bot build settlements.*)
+    actually have a firm grasp on the trophy (hence a value greater than .2) 
+    Adding another tenth a point seems reasonable, given that roads do have 
+    some strategic value, and in general, its probably good to encourage the 
+    bot to build roads.*)
     in
     let card_value = calc_card_value s in
-    let builds = [(cCOST_ROAD, cVP_ROAD); (cCOST_TOWN, cVP_TOWN); 
-          (cCOST_CITY, cVP_CITY-cVP_TOWN); (cCOST_CARD, card_value)]
+    let builds = [(cCOST_ROAD, cVP_ROAD); (cCOST_TOWN, float cVP_TOWN); 
+          (cCOST_CITY, float (cVP_CITY-cVP_TOWN)); (cCOST_CARD, card_value)]
     in 
-    let values = List.map analyze_build builds
+    let values = List.map analyze_build builds in
     let f (b, w, o, g, l) (b_a, w_a, o_a, g_a, l_a) =
       (b +. b_a, w +. w_a, o +. o_a, g +. g_a, l +. l_a)
     in
@@ -159,10 +149,8 @@ module Bot = functor (S : Soul) -> struct
     approximately represent the number of victory points each player has
   *)
 
-  let eval_state (s : state) : (float * float * float * float) =
+  let eval_state (s : state) (c: color) : (float * float * float * float) =
     failwith "not implemented"
-
-  let eval_resources 
 
   (* Invalid moves are overridden in game *)
   let handle_request ((b,p,t,n) : state) : move =
@@ -187,7 +175,8 @@ module Bot = functor (S : Soul) -> struct
     | ActionRequest -> 
       if is_none t.dicerolled then Action(RollDice) 
       else failwith "not implemented"
-      (*I'll first need to ponder some trades*)
+      (*I'll first need to ponder some trades.
+        Here, I think the best solution may be to*)
       (*Then I'll need to focus on what makes sense to buy*)
 
     
